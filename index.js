037e38908d76c7ee5f78883b9bcd5b7141827d75
danish-ibrahim-khan <143989917+danish-ibrahim-khan@users.noreply.github.com>
2024-12-14 16:49:39 +0530

    some changes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/',(req, res)=>{
 // })
 
 sequelize.sync({ force: false }).then(() => {
-    console.log("Database synced hello");
+    console.log("Database synced hello samo");
     
   });
   
